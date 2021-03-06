349f0498aeaf26dfc00569818b3d7013412193e2
Martin Dennhardt <martin@dennhardt.de>
2021-03-06 23:10:47 +0000

    Remove redis caching

--- abfuhrtermine/services/cache.ts
+++ abfuhrtermine/services/cache.ts
@@ -1,46 +1,8 @@
 import NodeCache = require("node-cache");
-import { ClientOpts, RedisClient } from "redis";
-import { promisify } from "util";
-
-class AsyncRedisClient extends RedisClient {
-    GETAsync = promisify(this.GET);
-    SETEXAsync = promisify(this.SETEX);
-}
 
 export interface ICache {
     getItem<T>(key: string): Promise<T>
     setItem<T>(key: string, value: T, seconds?: number): Promise<string>
-}
-
-export type RedisCacheOptions = ClientOpts & {
-    defaultTTL?: number;
-}
-
-export class RedisCache implements ICache {
-    private redisClient: AsyncRedisClient;
-    options: RedisCacheOptions;
-
-    constructor(options: RedisCacheOptions) {
-        this.options = Object.assign(options, {
-            defaultTTL: options.defaultTTL ?? 10
-        });
-        this.redisClient = new AsyncRedisClient(this.options);
-    }
-    async getItem<T>(key: string) {
-        return JSON.parse(await this.redisClient.GETAsync(key)) as T;
-    }
-    setItem<T>(key: string, value: T, seconds?: number) {
-        return this.redisClient.SETEXAsync(key, seconds ?? this.options.defaultTTL, JSON.stringify(value));
-    }
-}
-
-export class EmptyCache implements ICache {
-    async getItem<T>(key: string): Promise<T> {
-        return null;
-    }
-    async setItem<T>(key: string, value: T, seconds?: number): Promise<string> {
-        return "OK";
-    }
 }
 
 export class InMemoryCache implements ICache {
--- abfuhrtermine/services/index.ts
+++ abfuhrtermine/services/index.ts
@@ -1,16 +1,9 @@
 import { EbbWeb } from "./ebbweb"
-import { EmptyCache, InMemoryCache, RedisCache } from "./cache"
+import { InMemoryCache } from "./cache"
 import { Abfuhrtermine } from "./abfuhrtermine"
 import { Logger } from "@azure/functions";
 
 const ebbweb = new EbbWeb;
-// const cache = new RedisCache({
-//     host: process.env['REDIS_HOST'],
-//     port: Number(process.env['REDIS_PORT']),
-//     password: process.env['REDIS_PASSWORD'],
-//     defaultTTL: 21600
-// });
-// const cache = new EmptyCache;
 const cache = new InMemoryCache(21600);
 
 export const abfuhrtermine = (log: Logger) => new Abfuhrtermine(ebbweb, cache, log);
