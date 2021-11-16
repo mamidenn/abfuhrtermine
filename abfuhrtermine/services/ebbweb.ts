b41f922e483c38cac4b21d9f6b8a447494b08edb
Martin Dennhardt <martin@dennhardt.de>
2021-11-16 15:45:24 +0100

    Adapt to newly added year selection in ebbweb

--- abfuhrtermine/services/ebbweb.ts
+++ abfuhrtermine/services/ebbweb.ts
@@ -3,75 +3,95 @@ import * as moment from "moment";
 import { Either, left, right } from "fp-ts/lib/Either";
 
 export type Abfuhrkalender = {
-    straße: string,
-    nummer: string,
-    stand: Date,
-    restmüll: Date[],
-    bio: Date[],
-    papier: Date[],
-    gelberSack: Date[]
+  straße: string;
+  nummer: string;
+  stand: Date;
+  restmüll: Date[];
+  bio: Date[];
+  papier: Date[];
+  gelberSack: Date[];
 };
 
-export type HttpError = { code: number, message: string }
+export type HttpError = { code: number; message: string };
 
 export class EbbWeb {
-    async getDates(street: string, number: string): Promise<Either<HttpError, Abfuhrkalender>> {
-        const browser = await puppeteer.launch(
-            { 
-                headless: true,
-                args: [
-                    "--disable-gpu",
-                    "--disable-dev-shm-usage",
-                    "--disable-setuid-sandbox",
-                    "--no-sandbox"
-                ] 
-            }
-        );
-        const page = await browser.newPage();
-        const response = await page.goto('https://ebbweb.stadt.bamberg.de/WasteManagementBamberg/WasteManagementServlet?SubmitAction=wasteDisposalServices&InFrameMode=TRUE');
-        if (!response.ok()) {
-            throw new Error(`Got response status ${response.status()} from ebbweb`)
-        }
+  async getDates(
+    street: string,
+    number: string
+  ): Promise<Either<HttpError, Abfuhrkalender>> {
+    const result = {
+      straße: street,
+      nummer: number,
+      stand: moment().toDate(),
+      restmüll: [],
+      bio: [],
+      papier: [],
+      gelberSack: [],
+    };
 
-        await Promise.all([
-            page.waitForNavigation(),
-            page.select('select[name=Ort]', street.substr(0, 1))
-        ])
-        await page.select('select[name=Strasse]', street.replace(' ', '\xa0'));
-        await page.type('input[name=Hausnummer]', number);
-        await Promise.all([
-            page.waitForNavigation(),
-            page.click('a[name=forward]'),
-        ]);
-
-        if (!await page.$('div#termineblock')) {
-            return left({
-                code: 422,
-                message: `ebbweb did not yield results for Straße ${street}, Nummer: ${number}`
-            })
-        }
-
-        const getDatesFromColumn = async (selector: string) =>
-            (await page.$$eval(selector, elements => elements.map(element => element.textContent)))
-                .map(content => moment(content.substr(4, 10), 'DD.MM.YYYY').toDate());
-
-        const [restmüll, bio, papier, gelberSack] = await Promise.all([
-            getDatesFromColumn('td.workRest'),
-            getDatesFromColumn('td.workBio'),
-            getDatesFromColumn('td.workPapier'),
-            getDatesFromColumn('td.workGS')
-        ]);
-
-        await browser.close();
-
-        return right({
-            straße: street,
-            nummer: number,
-            stand: moment().toDate(),
-            restmüll: restmüll,
-            bio: bio,
-            papier: papier,
-            gelberSack: gelberSack
-        })
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+          "--disable-gpu",
+          "--disable-dev-shm-usage",
+          "--disable-setuid-sandbox",
+          "--no-sandbox"
+      ]
+    });
+    const page = await browser.newPage();
+    const response = await page.goto(
+      "https://ebbweb.stadt.bamberg.de/WasteManagementBamberg/WasteManagementServlet?SubmitAction=wasteDisposalServices&InFrameMode=TRUE"
+    );
+    if (!response.ok()) {
+      throw new Error(`Got response status ${response.status()} from ebbweb`);
     }
+
+    const getDatesFromColumn = async (selector: string) =>
+      (
+        await page.$$eval(selector, (elements) =>
+          elements.map((element) => element.textContent)
+        )
+      ).map((content) => moment(content.substr(4, 10), "DD.MM.YYYY").toDate());
+
+    const zeiträume = await page.$$eval(
+      "[name=Zeitraum]",
+      (elements: HTMLInputElement[]) => elements.map((element) => element.value)
+    );
+
+    for (const zeitraum of zeiträume) {
+      const radio = await page.$(`[name=Zeitraum][value="${zeitraum}"]`);
+      await radio.click();
+
+      await Promise.all([
+        page.waitForNavigation(),
+        page.select("select[name=Ort]", street.substr(0, 1)),
+      ]);
+      await page.select("select[name=Strasse]", street.replace(" ", "\xa0"));
+      const numberInput = await page.$("input[name=Hausnummer]");
+      await numberInput.click({ clickCount: 3 });
+      await numberInput.type(number);
+      await Promise.all([
+        page.waitForNavigation(),
+        page.click("a[name=forward]"),
+      ]);
+
+      if (!(await page.$("div#termineblock"))) {
+        return left({
+          code: 422,
+          message: `ebbweb did not yield results for Straße ${street}, Nummer: ${number}`,
+        });
+      }
+
+      result.restmüll.push(...(await getDatesFromColumn("td.workRest")));
+      result.bio.push(...(await getDatesFromColumn("td.workBio")));
+      result.papier.push(...(await getDatesFromColumn("td.workPapier")));
+      result.gelberSack.push(...(await getDatesFromColumn("td.workGS")));
+
+      await Promise.all([page.waitForNavigation(), page.click("a[name=back]")]);
+    }
+
+    await browser.close();
+
+    return right(result);
+  }
 }
